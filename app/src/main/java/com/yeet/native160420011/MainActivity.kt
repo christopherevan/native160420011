1fcd16999395d12d53c3ea22eee8d91152536b28
Christopher Evan <christopher.evan0603@gmail.com>
2023-03-01 10:26:26 +0700

    init

---
+++ app/src/main/java/com/yeet/native160420011/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yeet.native160420011
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
