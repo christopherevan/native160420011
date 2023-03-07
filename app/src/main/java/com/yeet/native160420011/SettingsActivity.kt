7138505b7751efbc7a8a76292601e9a0d8a4557c
Christopher Evan <christopher.evan0603@gmail.com>
2023-03-07 17:29:14 +0700

    bagian settings

---
+++ app/src/main/java/com/yeet/native160420011/SettingsActivity.kt
@@ -0,0 +1,17 @@
+package com.yeet.native160420011
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+
+    // TODO bikin settings
+
+    fun settings() {
+        return
+    }
+}
