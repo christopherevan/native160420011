e9eb155aba2a48e8f991190e93491c29eade9de6
Kecambah123 <119327983+Kecambah123@users.noreply.github.com>
2023-03-07 17:39:54 +0700

    register activity

---
+++ app/src/main/java/com/yeet/native160420011/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.yeet.native160420011
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    // TODO: create register feature
+}
